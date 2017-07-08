71dd747359d9ca2978ab8bd95645ab21819a64eb
Pietro Battilana <battilanap@gmail.com>
2017-07-08 16:34:36 +0200

    deck shuffling and dealing

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,19 @@
 package main;
 
+import decks.forty.Card;
 import decks.forty.Deck;
+import decks.forty.Hand;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Deck italianDeck = new Deck();
+
+        Hand hand = new Hand(italianDeck.shuffle().deal(3));
+
 
     }
 }
