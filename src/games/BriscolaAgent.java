8bdfc8eca19c9235e97259262770df8065a2aa2c
Pietro Battilana <battilanap@gmail.com>
2017-07-10 22:35:39 +0200

    briscola full game input Human vs (stupid) AI and score calculation

--- src/games/BriscolaAgent.java
+++ src/games/BriscolaAgent.java
@@ -1,4 +1,16 @@
 package games;
 
+import decks.forty.Card;
+
 public class BriscolaAgent {
+
+    private BriscolaEngine engine;
+
+    public BriscolaAgent(BriscolaEngine engine) {
+        this.engine = engine;
+    }
+
+    public Card makePly() {  // TODO
+        return engine.getManoP1().getHand().get(0);
+    }
 }
--- src/main/MainFortyDeck.java
+++ src/main/MainFortyDeck.java
@@ -1,6 +1,7 @@
 package main;
 
 import decks.forty.*;
+import games.BriscolaAgent;
 import games.BriscolaEngine;
 
 
@@ -11,7 +12,54 @@ public class MainFortyDeck {
 
     public static void main(String[] args) {
 
-        playBriscola();
+        playBriscolaAI();
+
+    }
+
+    private static void playBriscolaAI() {
+
+        BriscolaEngine briscolaEngine = new BriscolaEngine();
+        BriscolaAgent briscolaAgent = new BriscolaAgent(briscolaEngine);
+        Scanner sc = new Scanner(System.in);
+
+        Card cardSelected = new Card(Rank.ASSO, Suit.DENARI);
+        boolean isGameOver = false;
+
+        System.out.println("Game started. Enter the number of the card you want to play\n");
+
+        do {
+            System.out.println(briscolaEngine);
+
+            if (briscolaEngine.whoseTurn() == 1) {
+                cardSelected = briscolaAgent.makePly();
+
+            } else if (briscolaEngine.whoseTurn() == 2) {
+                int inputIDX = sc.nextInt();
+
+                if (inputIDX == 4) {
+                    Hand puntiP1 = briscolaEngine.getPuntiP1();
+                    System.out.println("\npoints P1 (" + briscolaEngine.pointScored(puntiP1) + ") " + puntiP1);
+                    continue;
+
+                } else if (inputIDX == 5) {
+                    Hand puntiP2 = briscolaEngine.getPuntiP2();
+                    System.out.println("\npoints P2 (" + briscolaEngine.pointScored(puntiP2) + ") " + puntiP2);
+                    continue;
+
+                } else if (inputIDX < 1 || inputIDX > 5
+                        || (briscolaEngine.getTurnsLeft() == 2 && inputIDX > 2)
+                        || (briscolaEngine.getTurnsLeft() == 1 && inputIDX > 1) ) {
+                    System.err.println("Entered out of range card number ('4'-'5' to see current points)");
+                    continue;
+
+                }
+                cardSelected = briscolaEngine.getManoP2().getHand().get(inputIDX - 1);
+
+            }
+
+            isGameOver = briscolaEngine.play(cardSelected);
+
+        } while (isGameOver == false);
 
     }
 
